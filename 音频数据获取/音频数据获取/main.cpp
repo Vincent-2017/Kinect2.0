11205a6ea651abde170804053880bb55b812374d
VincentZHOU <1156767986@qq.com>
2017-07-24 17:28:37 +0800

    这个不是获得音频数据，而是以stream的形式获得音频流

--- 音频数据获取/音频数据获取/main.cpp
+++ 音频数据获取/音频数据获取/main.cpp
@@ -6,20 +6,20 @@
 int main()
 {
 	printf("Hello, Wellcome to kinect world!\n");
-	IKinectSensor* bb; //申请一个Sensor指针  
-	HRESULT hr = GetDefaultKinectSensor(&bb); // 获取一个默认的Sensor  
+	IKinectSensor* pKinectSensor; //申请一个Sensor指针  
+	HRESULT hr = GetDefaultKinectSensor(&pKinectSensor); // 获取一个默认的Sensor  
 	if (FAILED(hr))
 	{
 		printf("No Kinect connect to your pc!\n");
 		goto endstop;
 	}
 	BOOLEAN bIsOpen = 0;
-	bb->get_IsOpen(&bIsOpen); // 查看下是否已经打开  
+	pKinectSensor->get_IsOpen(&bIsOpen); // 查看下是否已经打开  
 	printf("bIsOpen： %d\n", bIsOpen);
 
 	if (!bIsOpen) // 没打开，则尝试打开  
 	{
-		hr = bb->Open();
+		hr = pKinectSensor->Open();
 		if (FAILED(hr))
 		{
 			printf("Kinect Open Failed!\n");
@@ -31,63 +31,42 @@ int main()
 		Sleep(3000);
 	}
 	bIsOpen = 0;
-	bb->get_IsOpen(&bIsOpen); // 是否已经打开  
+	pKinectSensor->get_IsOpen(&bIsOpen); // 是否已经打开  
 	printf("bIsOpen： %d\n", bIsOpen);
 	BOOLEAN bAvaliable = 0;
-	bb->get_IsAvailable(&bAvaliable); // 是否可用  
+	pKinectSensor->get_IsAvailable(&bAvaliable); // 是否可用  
 	printf("bAvaliable： %d\n", bAvaliable);
 
 	DWORD dwCapability = 0;
-	bb->get_KinectCapabilities(&dwCapability); // 获取容量  
+	pKinectSensor->get_KinectCapabilities(&dwCapability); // 获取容量  
 	printf("dwCapability： %d\n", dwCapability);
 	TCHAR bbuid[256] = { 0 };
-	bb->get_UniqueKinectId(256, bbuid); // 获取唯一ID  
+	pKinectSensor->get_UniqueKinectId(256, bbuid); // 获取唯一ID  
 	printf("UID: %s\n", bbuid);
 
 	// 音频数据获取  
-	IAudioSource* audios = nullptr;
-	UINT nAudioCount = 0;
-	hr = bb->get_AudioSource(&audios);
+	IAudioSource* pAudioSource = nullptr;
+	// 音频帧读取器
+	IAudioBeamFrameReader*  pAudioBeamFrameReader = nullptr;
+	// 音频帧引用
+	IAudioBeamFrameReference* pABFrameRef = nullptr;
+	// 音频帧链表
+	IAudioBeamFrameList* pAudioBeamFrameList = nullptr;
+	// 音频帧
+	IAudioBeamFrame* pAudioBeamFrame = nullptr;
+	hr = pKinectSensor->get_AudioSource(&pAudioSource);
 	if (FAILED(hr))
 	{
 		printf("Audio Source get failed!\n");
 		goto endclose;
 	}
-	IAudioBeam* audiobm = nullptr;
-	IAudioBeamList* audiobml = nullptr;
-	audios->get_AudioBeams(&audiobml);
-	audiobml->OpenAudioBeam(0, &audiobm); // 目前只支持第一个  
-	IStream* stm = nullptr;
-	audiobm->OpenInputStream(&stm);
-	audios->Release();
-	audiobm->Release();
-
-	float fAngle = 0.0f;
-	float fAngleConfidence = 0.0f;
-	ULONG lRead = 0;
-	const ULONG lBufferSize = 3200;
-	float* fDataArr = new float[lBufferSize];
-	while (true)
-	{
-		fAngle = 0.0f;
-		fAngleConfidence = 0.0f;
-		audiobm->get_BeamAngle(&fAngle); // 获取音频的角度，[ -0.872665f, 0.8726665f ]  
-		audiobm->get_BeamAngleConfidence(&fAngleConfidence); // 获取音频的可信度（0 - 1）  
-		if (fAngleConfidence > 0.5f)
-			printf("Angle: %3.2f (%1.2f)\n", (fAngle)*180.0f / static_cast<float>(M_PI), fAngleConfidence);
-		// audio  data  
-		lRead = 0;
-		memset(fDataArr, 0, lBufferSize);
-		stm->Read(fDataArr, lBufferSize, &lRead);
-		if (lRead > 0)
-		{
-			printf("Audio Buffer: %d\n", lRead);
-		}
-		Sleep(200);
+	// 再获取音频帧读取器
+	if (SUCCEEDED(hr)){
+		hr = pAudioSource->OpenReader(&pAudioBeamFrameReader);
 	}
-
+	
 endclose:
-	bb->Close();
+	pKinectSensor->Close();
 endstop:
 	system("pause");
 	return 0;
