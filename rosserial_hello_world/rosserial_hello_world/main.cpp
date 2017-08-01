f8d5c0fce162ce532d0b0f7c88e2f52bd01f76c6
VincentZHOU <1156767986@qq.com>
2017-08-02 01:40:30 +0800

    可以连接ros系统，ip地址填写ubuntu系统的ip

---
+++ rosserial_hello_world/rosserial_hello_world/main.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <stdio.h>
+#include "ros.h"
+#include <geometry_msgs/Twist.h>
+#include <windows.h>
+
+using std::string;
+
+int main()
+{
+	ros::NodeHandle nh;
+	char *ros_master = "192.168.1.102";
+
+	printf("Connecting to server at %s\n", ros_master);
+	nh.initNode(ros_master);
+
+	printf("Advertising cmd_vel message\n");
+	geometry_msgs::Twist twist_msg;
+	ros::Publisher cmd_vel_pub("cmd_vel", &twist_msg);
+	nh.advertise(cmd_vel_pub);
+
+	printf("Go robot go!\n");
+	while (1)
+	{
+		twist_msg.linear.x = 0.4;
+		twist_msg.linear.y = 0;
+		twist_msg.linear.z = 0;
+		twist_msg.angular.x = 0;
+		twist_msg.angular.y = 0;
+		twist_msg.angular.z = 0.2;
+		cmd_vel_pub.publish(&twist_msg);
+
+		nh.spinOnce();
+		Sleep(100);
+	}
+
+	printf("All done!\n");
+	return 0;
+}
