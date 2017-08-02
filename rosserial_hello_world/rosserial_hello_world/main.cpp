268d86a2eb0f6848ef22446071dfc79758992842
VincentZHOU <1156767986@qq.com>
2017-08-02 19:28:41 +0800

    增加了控制功能，控制机器人前后，左右运动

--- rosserial_hello_world/rosserial_hello_world/main.cpp
+++ rosserial_hello_world/rosserial_hello_world/main.cpp
@@ -3,13 +3,24 @@
 #include "ros.h"
 #include <geometry_msgs/Twist.h>
 #include <windows.h>
+#include <opencv2\imgproc.hpp>	//opencv头文件
+#include <opencv2\calib3d.hpp>
+#include <opencv2\highgui.hpp>
+
+using namespace cv;
+using namespace std;
+
+#define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0) //必要的，我是背下来的
+
+bool flag = false;
 
 using std::string;
+
 
 int main()
 {
 	ros::NodeHandle nh;
-	char *ros_master = "192.168.137.9";
+	char *ros_master = "192.168.137.5";
 
 	printf("Connecting to server at %s\n", ros_master);
 	nh.initNode(ros_master);
@@ -22,12 +33,52 @@ int main()
 	printf("Go robot go!\n");
 	while (1)
 	{
-		twist_msg.linear.x += 0.2;
-		twist_msg.linear.y = 0;
-		twist_msg.linear.z = 0;
-		twist_msg.angular.x = 0;
-		twist_msg.angular.y = 0;
-		twist_msg.angular.z = 0;
+		if (KEY_DOWN('W'))
+		{
+			twist_msg.linear.x = 0.5;
+			twist_msg.linear.y = 0;
+			twist_msg.linear.z = 0;
+			twist_msg.angular.x = 0;
+			twist_msg.angular.y = 0;
+			twist_msg.angular.z = 0;
+		}
+		else if (KEY_DOWN('S'))
+		{
+			twist_msg.linear.x = -0.5;
+			twist_msg.linear.y = 0;
+			twist_msg.linear.z = 0;
+			twist_msg.angular.x = 0;
+			twist_msg.angular.y = 0;
+			twist_msg.angular.z = 0;
+		}
+		else if (KEY_DOWN('A'))
+		{
+			twist_msg.linear.x = 0;
+			twist_msg.linear.y = 0;
+			twist_msg.linear.z = 0;
+			twist_msg.angular.x = 0;
+			twist_msg.angular.y = 0;
+			twist_msg.angular.z = 0.5;
+		}
+		else if (KEY_DOWN('D'))
+		{
+			twist_msg.linear.x = 0;
+			twist_msg.linear.y = 0;
+			twist_msg.linear.z = 0;
+			twist_msg.angular.x = 0;
+			twist_msg.angular.y = 0;
+			twist_msg.angular.z = -0.5;
+		}
+		else 
+		{
+			twist_msg.linear.x = 0;
+			twist_msg.linear.y = 0;
+			twist_msg.linear.z = 0;
+			twist_msg.angular.x = 0;
+			twist_msg.angular.y = 0;
+			twist_msg.angular.z = 0;
+		}
+
 		cmd_vel_pub.publish(&twist_msg);
 
 		nh.spinOnce();
